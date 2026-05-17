cd7c747bbf38b3802d9a0edac7ef7b7b652cc06d
Ksenia <Xeonica01owl@gmail.com>
2026-05-17 17:51:29 +0300

    feat: implement FilmController with CRUD operations and validation

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,74 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
+
+    // Хранилище фильмов в памяти
+    private final Map<Long, Film> films = new HashMap<>();
+
+    private long currentId = 1;
+
+    // Константа: дата первого релиза (день рождения кино)
+    private static final LocalDate CINEMA_BIRTHDAY = LocalDate.of(1895, 12, 28);
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        log.info("Получен запрос на получение списка всех фильмов");
+        return films.values();
+    }
+
+    @PostMapping
+    public Film create(@Valid @RequestBody Film film) {
+        log.info("Получен запрос на создание фильма: {}", film);
+
+        validateFilm(film);
+
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+
+        log.info("Фильм успешно добавлен. Присвоен ID: {}", film.getId());
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@Valid @RequestBody Film film) {
+        log.info("Получен запрос на обновление фильма с ID: {}", film.getId());
+
+        if (film.getId() == null || !films.containsKey(film.getId())) {
+            log.error("Ошибка обновления: фильм с ID {} не найден", film.getId());
+            throw new ValidationException("Фильм с таким ID не найден");
+        }
+
+        validateFilm(film);
+
+        films.put(film.getId(), film);
+        log.info("Фильм с ID {} успешно обновлен", film.getId());
+        return film;
+    }
+
+    // Вспомогательный метод для валидации
+    private void validateFilm(Film film) {
+        if (film.getReleaseDate() != null && film.getReleaseDate().isBefore(CINEMA_BIRTHDAY)) {
+            log.error("Ошибка валидации: дата релиза {} раньше 28 декабря 1895 года", film.getReleaseDate());
+            throw new ValidationException("Дата релиза фильма не может быть раньше 28 декабря 1895 года");
+        }
+    }
+
+    // Вспомогательный метод генерации id
+    private long getNextId() {
+        return currentId++;
+    }
 }
