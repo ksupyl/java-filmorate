32b657c07611ad382dcf4a9d24e05e1e400736a5
Ksenia <Xeonica01owl@gmail.com>
2026-05-17 17:06:10 +0300

    feat: add User and Film models with basic validation

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,25 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private Long id;
+
+    @NotBlank(message = "Email не может быть пустым")
+    @Email(message = "Некорректный формат email")
+    private String email;
+
+    @NotBlank(message = "Логин не может быть пустым")
+    private String login;
+
+    private String name;
+
+    @PastOrPresent(message = "Дата рождения не может быть в будущем")
+    private LocalDate birthday;
+}
