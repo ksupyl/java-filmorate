9bd72ee1b70d22805e9d8d356ae4919d1d751882
Ksenia <Xeonica01owl@gmail.com>
2026-05-29 22:36:25 +0300

    fix: restore release date validation in InMemoryFilmStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -3,8 +3,10 @@ package ru.yandex.practicum.filmorate.storage.film;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,13 +15,16 @@ import java.util.Map;
 @Slf4j
 public class InMemoryFilmStorage implements FilmStorage {
 
+    // Константа: дата первого релиза (день рождения кино)
+    private static final LocalDate CINEMA_BIRTHDAY = LocalDate.of(1895, 12, 28);
+
     // Хранилище фильмов в памяти
     private final Map<Long, Film> films = new HashMap<>();
-
     private long currentId = 1;
 
     @Override
     public Film add(Film film) {
+        validateReleaseDate(film);
         film.setId(currentId++);
         films.put(film.getId(), film);
         log.debug("Добавлен фильм: id={}, name={}", film.getId(), film.getName());
@@ -32,6 +37,7 @@ public class InMemoryFilmStorage implements FilmStorage {
             log.warn("Попытка обновить несуществующий фильм: id={}", film.getId());
             throw new NotFoundException("Фильм с id=" + film.getId() + " не найден");
         }
+        validateReleaseDate(film);
         films.put(film.getId(), film);
         log.debug("Обновлён фильм: id={}", film.getId());
         return film;
@@ -58,5 +64,15 @@ public class InMemoryFilmStorage implements FilmStorage {
             throw new NotFoundException("Фильм с id=" + id + " не найден");
         }
         return film;
+    }
+
+    // Валидация даты релиза — не может быть раньше дня рождения кино
+    private void validateReleaseDate(Film film) {
+        if (film.getReleaseDate().isBefore(CINEMA_BIRTHDAY)) {
+            log.warn("Некорректная дата релиза: {}", film.getReleaseDate());
+            throw new ValidationException(
+                    "Дата релиза не может быть раньше 28 декабря 1895 года"
+            );
+        }
     }
 }
