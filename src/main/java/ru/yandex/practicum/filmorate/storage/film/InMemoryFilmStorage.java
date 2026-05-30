4c572b99201919af43a955087cc74c7e273f217e
Ksenia <Xeonica01owl@gmail.com>
2026-05-30 14:02:44 +0300

    style: replace Collectors.toList() with Stream.toList() in findPopular

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Component
 @Slf4j
@@ -57,6 +61,6 @@ public class InMemoryFilmStorage implements FilmStorage {
         return films.values().stream()
                 .sorted((f1, f2) -> f2.getLikes().size() - f1.getLikes().size())
                 .limit(count)
-                .collect(java.util.stream.Collectors.toList());
+                .toList();
     }
 }
